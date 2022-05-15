fb13110e98176a25a9a633a2f8c45441bdb644a6
khushisuri7 <76581264+khushisuri7@users.noreply.github.com>
2022-05-15 09:16:27 +0530

    Update main.js

--- OpenOcean/main.js
+++ OpenOcean/main.js
@@ -3,17 +3,13 @@ var fs = require('fs');
 
 
 http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
+
    response.writeHead(200, {'Content-Type': 'text/html'});
    var file = fs.createReadStream('index.html');
    file.pipe(response);
-   // Send the response ody as "Hello World"
-  // response.end('Hello World\n');
+
 }).listen(8081);
 
-// Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
 
 
@@ -23,16 +19,6 @@ var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 web3.eth.getAccounts(console.log);
 var Contract = require('web3-eth-contract');
 
-// set provider for all later instances to use
 Contract.setProvider('ws://localhost:8545');
 
 var contract = new Contract(jsonInterface, address);
-
-
-//get contract
-//deposit
-//withdraw
-//Display Ether Price
-//Display Vault Amount
-
-

3ed56a8293b989da532f35c3c81e7a25931b1cd9
Rrs-4817 <82603514+Rrs-4817@users.noreply.github.com>
2022-05-15 10:34:54 +0530

    Update PriceConsumerV3.sol

--- OpenOcean/contracts/implementations/PriceConsumerV3.sol
+++ OpenOcean/contracts/implementations/PriceConsumerV3.sol
@@ -6,18 +6,12 @@ contract PriceConsumerV3 {
 
     AggregatorV3Interface internal priceFeed;
 
-    /**
-     * Network: Kovan
-     * Aggregator: ETH/USD
-     * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
-     */
+  
     constructor() {
         priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
     }
 
-    /**
-     * Returns the latest price
-     */
+ 
     function getLatestPrice() public view returns (int) {
         (
             uint80 roundID, 
